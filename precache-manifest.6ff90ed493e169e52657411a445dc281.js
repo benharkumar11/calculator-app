4270431699948610a6c20edd1780ce89b7d0fe3b
benharkumar <benharkumar11@gmail.com>
2025-01-17 19:19:27 -0500

    Updates

---
+++ precache-manifest.6ff90ed493e169e52657411a445dc281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2280c5b424698873f8b9bb23f42cb18",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "70d9668b5cd1e02f201a",
+    "url": "/calculator-app/static/css/main.5c55ec1f.chunk.css"
+  },
+  {
+    "revision": "b2fdae28738b70a2436c",
+    "url": "/calculator-app/static/js/2.afe57e56.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/calculator-app/static/js/2.afe57e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d9668b5cd1e02f201a",
+    "url": "/calculator-app/static/js/main.6937833f.chunk.js"
+  },
+  {
+    "revision": "20b67ce09d39a151f121",
+    "url": "/calculator-app/static/js/runtime-main.03d8a58a.js"
+  }
+]);
